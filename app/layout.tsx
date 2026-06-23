b1e09f35181e7617e2ff69855700a700f205f6df
saptahadiwijaya7 <110339929+saptahadiwijaya7@users.noreply.github.com>
2026-06-23 22:44:17 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "SpinWheel Seminar",
+  title: "SpinWil Cihuy",
   description: "Aplikasi undian spinwheel untuk seminar dan event."
 };
 
