36c57f779acfae5d33468b7130d09962dd39862d
saptahadiwijaya7 <110339929+saptahadiwijaya7@users.noreply.github.com>
2026-06-25 13:46:57 +0700

    Change metadata for Spinwil Cihuy application
    Updated metadata title and description, and added favicon icon.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,11 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "SpinWheel Seminar",
-  description: "Aplikasi undian spinwheel untuk seminar dan event."
+  title: "Spinwil Cihuy",
+  description: "Aplikasi undian spinwheel untuk apapun.",
+  icons: {
+    icon: "/img/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
